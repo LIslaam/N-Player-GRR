23a3834fad482fc2d917a2a7980af3c653645bec
Labeebah <labeebah.sahar@gmail.com>
2023-08-16 13:37:57 +0000

    update

--- src/run_exp.py
+++ src/run_exp.py
@@ -1,7 +1,6 @@
-import pkg_resources
-pkg_resources.require("jax==0.2.22")
-pkg_resources.require("jaxlib==0.1.76")
-import os
+#import pkg_resources
+#pkg_resources.require("jax==0.2.22")
+#pkg_resources.require("jaxlib==0.1.76")
 import jax
 import jax.numpy as jnp
 import numpy as np
@@ -9,13 +8,14 @@ import argparse
 from lyap_2d import do_2d_lyapunov
 from lyap_3d import do_3d_lyapunov
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+print(jax.devices())
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--game", type=str, required=True)
 parser.add_argument("--optimiser", type=str, required=True)
+parser.add_argument("--lr", type=float, default=5.)
 parser.add_argument("--seed", type=int, default=0)
-parser.add_argument("--num_bin", type=int, default=11)
+parser.add_argument("--num_bins", type=int, default=11)
 parser.add_argument("--print", type=bool, default=True)
 parser.add_argument("--dimensions", type=int, default=2) # 2D Lyapunov or 3D Lyapunov
 args = parser.parse_args()
@@ -24,15 +24,15 @@ args = parser.parse_args()
 if __name__ == "__main__":
     ############################################
     seed = args.seed
-    num_bin = args.num_bin
+    num_bins = args.num_bins
     do_print = args.print
     game = args.game
     opt = args.optimiser
     dims = args.dimensions
+    alpha = args.lr
     
     mix_coeff = 0.25
     gamma = 0.9
-    alpha = 1.0
     ax_key = 'grad'
     d_strategy = 'do_ub'
     num_directions = 1
@@ -45,16 +45,26 @@ if __name__ == "__main__":
     use_smart_dir=False
     do_trajectories=True
 
-    if game == 'small-ipd':
-        game_labels = 'Small IPD'
-    elif game == 'mix':
-        game_labels = 'Mixed Small IPD and MP'
-    elif game == 'mp':
-        game_labels = 'Matching Pennies'
+
+    if args.dimensions == 2:
+        if game == 'small-ipd':
+            game_labels = 'Small IPD'
+        elif game == 'mix':
+            game_labels = 'Mixed Small IPD and MP'
+        elif game == 'mp':
+            game_labels = 'Matching Pennies'
+        else:
+            raise NotImplementedError
+        
+    elif args.dimensions == 3:
+        if game == 'offense-defense' or game=='o-d':
+            game_labels = 'Offense-Defense'
+        else:
+            raise NotImplementedError
 
     if opt == 'sgd':
         opt_labels = 'SGD'
-    elif opt == 'lola':
+    elif opt == 'lola' or opt=='LOLA':
         opt_labels = 'LOLA'
     
 
@@ -71,17 +81,17 @@ if __name__ == "__main__":
     else: assert False, 'Use a valid d_strategy'
 
     print(f"\t\t\t\t\t d_stratregy = {d_strategy}")
-    save_name = 'test_thetas_2d' # f'{game_labels[i]} {opt_labels[j]} $lr=${alpha}' #{alpha}' #{num_lyapunov_iters}-step Trajectories'
+    save_name = f'{game_labels} {opt_labels} $lr=${alpha}' #{alpha}' #{num_lyapunov_iters}-step Trajectories'
     #f"Game={game}_Opt={opt}_SigRange={do_sigmoid_range}_Key={temp_ax_key}_Num={num_lyapunov_iters}_dstrat={d_strategy}"
     title = f'{game_labels} {opt_labels} $lr=${alpha}'
 
     if dims==2:
-        levels = do_2d_lyapunov(game, seed, mix_coeff, gamma, alpha, opt, num_bin, ax_key, num_directions,
+        levels = do_2d_lyapunov(game, seed, mix_coeff, gamma, alpha, opt, num_bins, ax_key, num_directions,
                                 do_sigmoid_range, num_lyapunov_iters, save_name, title, do_print, do_legend,
                                 tune_first_dir, tune_every_dir, use_smart_dir, do_trajectories)
         
     if dims==3:
-        levels = do_3d_lyapunov(game, seed, mix_coeff, gamma, alpha, opt, num_bin, ax_key, num_directions,
+        levels = do_3d_lyapunov(game, seed, mix_coeff, gamma, alpha, opt, num_bins, ax_key, num_directions,
                                 do_sigmoid_range, num_lyapunov_iters, save_name, title, do_print, do_legend,
                                 tune_first_dir, tune_every_dir, use_smart_dir, do_trajectories)        
         
---
+++ src/test_gpu.py
@@ -0,0 +1,10 @@
+import jax
+import jaxlib
+import jax.numpy as jnp
+
+print(jax.__version__)
+print(jaxlib.__version__)
+
+print(jax.devices())
+
+jax.grad(jnp.linalg.det)(jnp.eye(2))
